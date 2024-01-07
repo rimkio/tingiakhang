994f24dc1bd1a30f16d4d789d86c089311ee684d
Rim <bvt.vanthang2010@gmail.com>
2024-01-07 10:28:03 +0700

    update

--- template-parts/footer.php
+++ template-parts/footer.php
@@ -27,10 +27,10 @@ $position = get_field("position", "option");
                         <h5><span style="text-transform:uppercase">Tín Gia Khang</span></h5>
                         <p><strong>Address: </strong> <?= $address ?></p>
                         <p>
-                            <strong>Telephone: </strong> <a style="color:#000000" href="tel:<?= $phone ?>"><?= $phone ?></a>
+                            <strong>Hotline: </strong> <a style="color:#000000" href="tel:<?= $phone ?>"><?= $phone ?></a>
                         </p>
                         <?php if ($phone_2) { ?>
-                            <p><strong>Telephone 2: </strong> <a style="color:#000000" href="tel:<?= $phone_2 ?>"><?= $phone_2 ?></a></p>
+                            <p><strong>Telephone: </strong> <a style="color:#000000" href="tel:<?= $phone_2 ?>"><?= $phone_2 ?></a></p>
                         <?php } ?>
                         <p><strong>Email:</strong> <a style="color:#000000" href="mailto:<?= $email_staff ?>"><?= $email_staff ?></a></p>
                     </div>
@@ -162,3 +162,17 @@ $position = get_field("position", "option");
     </div>
 
 </footer>
+
+<div id="contact_fixed">
+    <a target="_blank" href="tel:<?= $phone ?>" style="bottom:20px" title="Vui lòng gọi chúng tôi">
+        <div style="height:50px; width:50px; border-radius:50%; background:#64bc46;">
+            <p style="color:#FFFFFF; text-align:center; padding-top:6px"><svg width="64px" height="64px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#fff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M16.5562 12.9062L16.1007 13.359C16.1007 13.359 15.0181 14.4355 12.0631 11.4972C9.10812 8.55901 10.1907 7.48257 10.1907 7.48257L10.4775 7.19738C11.1841 6.49484 11.2507 5.36691 10.6342 4.54348L9.37326 2.85908C8.61028 1.83992 7.13596 1.70529 6.26145 2.57483L4.69185 4.13552C4.25823 4.56668 3.96765 5.12559 4.00289 5.74561C4.09304 7.33182 4.81071 10.7447 8.81536 14.7266C13.0621 18.9492 17.0468 19.117 18.6763 18.9651C19.1917 18.9171 19.6399 18.6546 20.0011 18.2954L21.4217 16.883C22.3806 15.9295 22.1102 14.2949 20.8833 13.628L18.9728 12.5894C18.1672 12.1515 17.1858 12.2801 16.5562 12.9062Z" fill="#fff"></path> </g></svg></p>
+        </div>
+    </a>
+    <a target="_blank" href="https://zalo.me/<?= $phone ?>" style="bottom:80px" title="Vui lòng liên hệ zalo">
+        <div style="height:50px; width:50px; border-radius:50%; background:#0d94e4;">
+            <p style="color:#FFFFFF; text-align:center; padding-top:12px; font-size:18px; font-weight:600">Zalo</p>
+        </div>
+    </a>
+
+</div>
